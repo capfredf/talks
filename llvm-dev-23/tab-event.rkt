b3c200beb2156ba5eb979c8dcff9e53a552259bd
Fred Fu <moonsolo@gmail.com>
2023-09-26 16:38:06 -0400

    ups

--- llvm-dev-23/tab-event.rkt
+++ llvm-dev-23/tab-event.rkt
@@ -5,10 +5,22 @@
 (provide (all-defined-out))
 
 (define (tab-event)
+  (define fst (cc-superimpose
+                (filled-rounded-rectangle 50 50 #:color "purple")
+                (text "Tab")))
+  (define snd (cc-superimpose
+                           (filled-rounded-rectangle 100 50 #:color "purple")
+                           (text "ReplListCompleter")))
+  (define combined (hc-append 100 fst snd))
   (pslide #:layout 'center
           (parameterize ([get-current-code-font-size (lambda () (- (current-font-size) 10))])
             (repl-input "struct WhateverMeaningfulLoooooooooongName { int field;};"
-                        "Wh⇥"))))
+                        "Wh⇥"))
+          (pin-arrow-line 10
+                          combined
+                          fst rc-find
+                          snd lc-find
+                          #:line-width 3)))
 
 (module+ main
   (tab-event))
