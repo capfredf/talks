33e2393ba8f5c5b0e979d037ed4bcac44958e038
Fred Fu <moonsolo@gmail.com>
2023-09-27 11:21:21 -0400

    up

--- llvm-dev-23/tab-event.rkt
+++ llvm-dev-23/tab-event.rkt
@@ -7,20 +7,25 @@
 
 (define (tab-event)
   (mp:current-inner-separation 0.2)
-  (define tenode (mp:rounded-rectangle-node "tab event" #:color "purple"))
+  (define line-editor-node (mp:rounded-rectangle-node "Line Editor Node" #:color "purple"))
+  (define completer-node (mp:rounded-rectangle-node "ReplListCompleter" #:color "purple" #:below line-editor-node))
   (define operator (mp:rounded-rectangle-node
                     "operator()(StringRef Buffer, size_t Pos)"
-                    #:color "purple" #:right-of tenode))
-  (define completer-node (mp:rounded-rectangle-node "ReplListCompleter" #:color "purple" #:above operator))
+                    #:color "purple" #:below completer-node))
+  (define tenode (mp:rounded-rectangle-node "tab event" #:color "purple" #:left-of operator))
+  (define meat-node (mp:rectangle-node "THE MEAT" #:color "red" #:below operator))
+  (define results (mp:rounded-rectangle-node "Results" #:color "purple" #:below tenode))
 
+  (define e0 (mp:edge completer-node line-editor-node))
   (define e1 (mp:edge tenode operator))
   (define e2 (mp:edge completer-node operator))
-  (pslide #:layout 'center
+  (define e3 (mp:edge operator meat-node))
+  (define e4 (mp:edge meat-node results))
+  (pslide #:go (coord 0.5 0.4 'cb)
           (parameterize ([get-current-code-font-size (lambda () (- (current-font-size) 10))])
             (repl-input "struct WhateverMeaningfulLoooooooooongName { int field;};"
                         "Wh⇥"))
-          #:go (coord 0.3 0.85 'cb)
-          (mp:draw tenode completer-node operator e1 e2)))
+          (mp:draw line-editor-node tenode completer-node operator meat-node results e0 e1 e2 e3 e4)))
 
 (module+ main
   (tab-event))
