7340dd5d79a9807c7a21518fa7fd7b2c44c435ef
Fred Fu <moonsolo@gmail.com>
2023-10-03 16:49:01 -0400

    up

--- llvm-dev-23/motivations.rkt
+++ llvm-dev-23/motivations.rkt
@@ -11,18 +11,18 @@
 
 
 (define (motivations)
-  (pslide #:title "Motivations"
-          #:next
-          (item "Avoid Tedious Typing")
-          (repl-input "struct WhateverMeaningfulLoooooooooongName{ int field;};"
-                      "Wh<tab>")
-          #:next
-          (item "Semantic Completion")
-          (repl-input "struct Apple{ int price;};"
-                      "struct Banana{ int StoreID;};"
-                      "void getApple(Apple &a) {};"
-                      "Apple fruitIsApple(10);"
-                      "Banana fruitIsBanana(42);"
-                      "getApple(f<tab>")))
+  (for ([i (list "Wh⇥" "WhateverMeaningfulLoooooooooongName" "WhateverMeaningfulLoooooooooongName")]
+        [j (list "getApple(f⇥" "getApple(f⇥" "getApple(fruitIsApple")])
+    (pslide #:title "Motivations"
+            (item "Avoid Tedious Typing")
+            (repl-input "struct WhateverMeaningfulLoooooooooongName{ int field;};"
+                        i)
+            (item "Semantic Completion")
+            (repl-input "struct Apple{ int price;};"
+                        "struct Banana{ int StoreID;};"
+                        "void getApple(Apple &a) {};"
+                        "Apple fruitIsApple(10);"
+                        "Banana fruitIsBanana(42);"
+                        j))))
 (module+ main
   (motivations))
