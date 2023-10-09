46c5595c8f42542767cc3a38da914c48595c3600
Fred Fu <moonsolo@gmail.com>
2023-10-09 16:17:52 -0400

    Fix

--- llvm-dev-23/demo.rkt
+++ llvm-dev-23/demo.rkt
@@ -5,20 +5,16 @@
 
 (define (demo)
   (for ([i (list "Wh⇥" "WhateverMeaningfulLoooooooooongName")])
-    (pslide #:title "Demo"
+    (pslide #:title "Basic Code Completion"
             #:go (coord 0.15 0.4 'lt)
-            (item "Avoid Tedious Typing")
-            #:go (coord 0.2 0.5 'lt)
             (repl-input "struct WhateverMeaningfulLoooooooooongName{ int field;};"
                         i)))
 
   (for ([j (list "pickOne(number1, ⇥" (list "pickOne(number1, █" "number1" "number2"))])
-    (pslide #:title "Demo"
+    (pslide #:title "Semantic Code Completion"
             #:go (coord 0.15 0.4 'lt)
-            (item "Semantic Completion")
-            #:go (coord 0.2 0.5 'lt)
             (repl-input/with-output "int number1 = 42, number2 = 84;"
-                                    "std::string name1 = \"Fred\", name2 = \"Yuquan\";"
+                                    "std::string name1 = \"Fred\", name2 = \"Vassil\";"
                                     "template <typename T> T pickOne(T v1, T v2) {...};"
                                     j))))
 (module+ main
